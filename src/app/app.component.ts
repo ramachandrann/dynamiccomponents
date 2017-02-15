5d15b959b0437585442da5aad2d241469736deb5
Gopal <gopalakrishnan.tr@gmail.com>
2017-02-15 10:51:24 -0500

    save

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ComponentFactoryResolver, NgModule, ViewChild, ViewContainerRef } from '@angular/core';
+import {DialogComponent} from './dialog.component'
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  // Note the use of the "read" option
+  @ViewChild('dialogAnchor', {read: ViewContainerRef}) dialogAnchor: ViewContainerRef;
+
+  constructor(private componentFactoryResolver: ComponentFactoryResolver) {}
+  
+  openDialogBox() {
+    // Close any already open dialogs
+    this.dialogAnchor.clear();
+    
+    let dialogComponentFactory = this.componentFactoryResolver.resolveComponentFactory(DialogComponent);
+    let dialogComponentRef = this.dialogAnchor.createComponent(dialogComponentFactory);
+    dialogComponentRef.instance.message = "hola, soy una caja de diálogo"; // Not sure about the translation here
+    dialogComponentRef.instance.close.subscribe(() => {
+      dialogComponentRef.destroy();
+    })
+    
+  }
+
 }
